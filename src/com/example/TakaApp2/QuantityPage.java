ddbc1b499dd135f080acc9c179881ca1a6c9d704
OliveChao <chaoolive@gmail.com>
2014-11-25 09:43:49 +0300

    Modified:QuantityPage java file added

--- src/com/example/TakaApp2/QuantityPage.java
+++ src/com/example/TakaApp2/QuantityPage.java
@@ -1,7 +1,16 @@
 package com.example.TakaApp2;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by akirachix on 11/20/14.
  */
-public class QuantityPage {
+public class QuantityPage extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_page);
+    }
 }
